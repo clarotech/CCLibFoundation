8f7a19fb0c61c7565624b71bff52446355375626
clarotech <clarotechuk@gmail.com>
2018-09-21 20:26:21 +0100

    First example code

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using ClaroTech.FHIRSTU3.CCProfilesFoundation;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
 
 namespace Examples
 {
@@ -6,7 +10,38 @@ namespace Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExampleCCOrganization();
+        }
+
+        static void ExampleCCOrganization()
+        {
+            Organization ex = new Organization();
+            ex.CCInit();   // Initialise the metadata
+            ex.Name = "Acme Example Organisation";
+
+            ex.CCAddOdsOrganisationCode("ABC123");  // Add an id with appropriate system id
+            ex.CCAddOdsSiteCode("XYZ999");   // Add an id with appropriate system id
+
+            ResourceReference dummy = new ResourceReference("http://acme.org/Location/dummy");
+            ex.CCSetMainLocation(dummy);   // Add CareConnect Extension
+
+            Period dummyPeriod = new Period(new FhirDateTime(2011,3,12), new FhirDateTime(2017,10,2));
+            ex.CCSetPeriod(dummyPeriod);  // Add CareConnect Extension
+
+            WriteFhirXml("organization.xml", ex);
+
+
+        }
+
+
+        static void WriteFhirXml(string filename, Resource res)
+        {
+            if (!Directory.Exists("ExampleFiles"))
+                Directory.CreateDirectory("ExampleFiles");
+
+            FhirXmlSerializer op = new FhirXmlSerializer();
+            string outxml = op.SerializeToString(res);
+            File.WriteAllText($"ExampleFiles\\{filename}", outxml);
         }
     }
 }
