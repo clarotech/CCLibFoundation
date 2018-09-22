b1efbd71394f4adf893029974e4237650b12cbc8
clarotech <clarotechuk@gmail.com>
2018-09-22 08:43:25 +0100

    Start Patient example

--- Examples/Examples/Program.cs
+++ Examples/Examples/Program.cs
@@ -1,19 +1,51 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using ClaroTech.FHIRSTU3.CCProfilesFoundation;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
 
 namespace Examples
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             ExampleCCOrganization();
+            ExampleCCPatient();
         }
 
-        static void ExampleCCOrganization()
+        private static void ExampleCCPatient()
+        {
+            Patient pat = new Patient();
+            pat.CCInit();  // Initialise the metadata
+
+            // Add some basic data for the exampe
+            pat.Name.Add(
+                new HumanName()
+                {
+                    Given = new []{ "John" },
+                    Family = "Smith",
+                    Use = HumanName.NameUse.Official
+                });
+
+
+            // Add patient ethnic category
+            pat.CCSetEthnicCategory("A","British, Mixed British");
+
+
+            // Add a religios affiliation (a SNOMED CT code)
+            pat.CCSetReligiousAffiliation("900000000000508004", "Buddhist, follower of religion");
+
+            // State whether a donor or not
+            pat.CCSetCadavericDonor(false);
+
+
+
+
+            WriteFhirXml("patent.xml", pat);
+
+        }
+
+        private static void ExampleCCOrganization()
         {
             Organization ex = new Organization();
             ex.CCInit();   // Initialise the metadata
@@ -25,7 +57,7 @@ namespace Examples
             ResourceReference dummy = new ResourceReference("http://acme.org/Location/dummy");
             ex.CCSetMainLocation(dummy);   // Add CareConnect Extension
 
-            Period dummyPeriod = new Period(new FhirDateTime(2011,3,12), new FhirDateTime(2017,10,2));
+            Period dummyPeriod = new Period(new FhirDateTime(2011, 3, 12), new FhirDateTime(2017, 10, 2));
             ex.CCSetPeriod(dummyPeriod);  // Add CareConnect Extension
 
             WriteFhirXml("organization.xml", ex);
@@ -33,11 +65,12 @@ namespace Examples
 
         }
 
-
-        static void WriteFhirXml(string filename, Resource res)
+        private static void WriteFhirXml(string filename, Resource res)
         {
             if (!Directory.Exists("ExampleFiles"))
+            {
                 Directory.CreateDirectory("ExampleFiles");
+            }
 
             FhirXmlSerializer op = new FhirXmlSerializer();
             string outxml = op.SerializeToString(res);
